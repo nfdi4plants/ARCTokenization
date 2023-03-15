4fadb9accef0f45de453da5d4605b314b717f5f7
muehlhaus <muehlhaus@bio.uni-kl.de>
2023-03-15 14:46:41 +0100

    Initial data model

---
+++ src/ArcGraphModel/CvParam.fs
@@ -0,0 +1,124 @@
+﻿namespace ArcGraphModel
+
+
+/// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+/// ----------------------------------------------------------------------------------------
+/// Controlled vocabulary (Cv) 
+/// ----------------------------------------------------------------------------------------
+
+
+
+module CvParam =
+    open System
+
+
+    type ModelDataType =
+        | Investigation
+        | Study
+        | Assay
+        | Person
+        | IsaCharacteristic  
+        | IsaFactor
+        | IsaParamter
+        | IsaSample
+        | IsaProtocolRef
+        | File
+        | Directory
+        | Custom of string 
+
+    type Adress =
+        // sheetname * row * col
+        | Table of string * int * int 
+
+    /// Represents a term from a controlled vocabulary (Cv)
+    /// in the form of: id|accession * name|value * refUri
+    // ?Maybe [<Struct>]
+    type CvTerm = string * string * string
+
+    /// Represents a unit term from the unit ontology 
+    /// in the form of: id|accession * name * refUri
+    // ?Maybe [<Struct>]
+    type CvUnit = string * string * string
+
+    /// Represent the different cases of a parameter, which is either a simple value,
+    /// a CvTerm or a simple value with CvUnit 
+    // ?Maybe [<Struct>]
+    type ParamValue<'T when 'T :> IConvertible> =
+        | Value of v: 'T 
+        // id|accession * name|value * ref
+        | CvValue of cv: CvTerm
+        // value * CvUnit
+        | WithCvUnitAccession of cvu : 'T * CvUnit
+
+    /// Interface ensures the propterties necessary for CvTerm 
+    type ICvBase =        
+        abstract member ID       : string    
+        abstract member Name     : string
+        abstract member RefUri   : string
+        // here or as node/edge abstraction 
+        // abstract member DataType   : ModelDataType
+        // abstract member Adress     : Adress / Location / Position
+
+    /// Interface ensures the value as ParamValue<'T>  
+    type IParamBase<'T when 'T :> IConvertible> =        
+        abstract member Value    : ParamValue<'T>    
+
+    /// Represents controlled vocabulary term as key for a ParamValue as value
+    type CvParam<'T when 'T :> IConvertible>(cvAccession:string,cvName:string,cvRefUri:string,paramValue:ParamValue<'T>) =
+        interface ICvBase with 
+            member this.ID     = cvAccession
+            member this.Name   = cvName
+            member this.RefUri = cvRefUri
+        interface IParamBase<'T> with 
+            member this.Value  = paramValue
+
+        member this.CvAccession = cvAccession
+
+    /// Represents user defined term as key for a ParamValue as value
+    type UserParam<'T when 'T :> IConvertible>(name:string,paramValue:ParamValue<'T>) =
+        interface ICvBase with 
+            member this.ID     = name
+            member this.Name   = name
+            member this.RefUri = "UserTerm"
+        interface IParamBase<'T> with         
+            member this.Value  = paramValue
+
+
+    type CvObject<'T>(cvAccession:string,cvName:string,cvRefUri:string,object:'T) =
+        interface ICvBase with 
+            member this.ID     = cvAccession
+            member this.Name   = cvName
+            member this.RefUri = cvRefUri    
+
+        member this.Object = object
+
+    type CvDoc<'T when 'T :> IParamBase<IConvertible> and 'T:> ICvBase>(cvAccession:string,cvName:string,cvRefUri:string,doc:'T list) =
+        interface ICvBase with 
+            member this.ID     = cvAccession
+            member this.Name   = cvName
+            member this.RefUri = cvRefUri    
+
+        member this.Document = doc
+
+    // Maybe a CvJDoc is necessary. Using SimpleJson as document type
+
+
+
+
+    let getCvAccessionOrName (param:#ICvBase) =
+        param.ID
+
+    let getValue (param:#IParamBase<_>) =
+        match param.Value with
+        | Value   v                 -> v
+        | CvValue (_,v,_)           -> v
+        | WithCvUnitAccession (v,_) -> v
+
+    let tryGetCvUnit  (param:#IParamBase<_>) : CvUnit option =
+        match param.Value with
+        | Value _                   -> None
+        | CvValue _                 -> None
+        | WithCvUnitAccession (_,u) -> Some u
+
+
+
--- src/ArcGraphModel/Library.fs
+++
@@ -1,5 +0,0 @@
-﻿namespace ArcGraphModel
-
-module Say =
-    let hello name =
-        printfn "Hello %s" name
