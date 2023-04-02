e17191429d485317d37cdf7231523895ac787ff3
omaus <omaus@bio.uni-kl.de>
2023-04-03 00:14:50 +0200

    Add Param unit tests & delete sample-test file

---
+++ tests/ArcGraphModel.Tests/ParamTests.fs
@@ -0,0 +1,18 @@
+﻿namespace ArcGraphMode.Tests
+
+open Xunit
+open ArcGraphModel.Param
+
+
+module Param =
+
+    let testParamValueValue = ParamValue.Value "ParamValue.Value"
+    let testCvParam = CvParam("TAN", "Name", "TSR", testParamValueValue)
+
+
+    module getCvAccession =
+
+        [<Fact>]
+        let ``returns correct TAN`` () =
+            let retrievedTan = getCvAccession testCvParam
+            Assert.Equal("TAN", retrievedTan)
