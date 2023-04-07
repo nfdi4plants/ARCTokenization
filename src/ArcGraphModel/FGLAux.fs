a7f5f10d9f0c7df3574bb436859e0f818c137dec
omaus <omaus@bio.uni-kl.de>
2023-04-07 08:49:01 +0200

    Add auxiliary functions for FGL

---
+++ src/ArcGraphModel/FGLAux.fs
@@ -0,0 +1,11 @@
+﻿/// Additional functions to work with FSharp.FGL graphs.
+module FGLAux
+
+open FSharp.FGL
+open FSharp.FGL.ArrayAdjacencyGraph
+
+/// <summary>
+/// Takes a graph and returns an array of its LVertices with keys and labels.
+/// </summary>
+let extractVerticesWithLabels (graph : ArrayAdjacencyGraph<'a,'b,'c>) : LVertex<'a,'b> [] =
+    (graph.GetVertices(), graph.GetLabels()) ||> Array.zip
