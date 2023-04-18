edf0b1c0ed29a3152a8f94d3ed1af0162d81b117
Heinrich Lukas Weil <hl.weil@t-online.de>
2023-04-18 10:15:22 +0200

    add helper functions and example isa type

--- src/ArcGraphModel/CvTokens/CvContainer.fs
+++ src/ArcGraphModel/CvTokens/CvContainer.fs
@@ -24,3 +24,20 @@ type CvContainer (cvAccession:string,cvName:string,cvRefUri:string,properties : 
         CvContainer(term,properties)
 
     new (term : CvTerm) = CvContainer (term,Seq.empty)  
+
+    static member getManyAs<'T when 'T :> ICvBase> propertyName (cvContainer : CvContainer) =
+        Dictionary.item propertyName cvContainer
+        |> Seq.map (fun cv -> cv :?> 'T)
+
+    static member getSingleAs<'T when 'T :> ICvBase> propertyName (cvContainer : CvContainer) =
+        Dictionary.item propertyName cvContainer
+        |> Seq.exactlyOne 
+        |> (fun cv -> cv :?> 'T)
+
+    static member setMany propertyName (values : seq<#ICvBase>) (cvContainer : CvContainer) =
+        Dictionary.addOrUpdateInPlace propertyName values cvContainer
+        |> ignore
+
+    static member setSingle propertyName (value : #ICvBase) (cvContainer : CvContainer) =
+        Dictionary.addOrUpdateInPlace propertyName (Seq.singleton value) cvContainer
+        |> ignore
--- src/ArcGraphModel/CvTokens/ParamValue.fs
+++ src/ArcGraphModel/CvTokens/ParamValue.fs
@@ -12,11 +12,30 @@ type ParamValue =
     // value * CvUnit
     | WithCvUnitAccession of cvu : IConvertible * CvUnit
 
-    static member getValue (param:ParamValue) =
+    //static member getValueAsStringWithUnit (param:ParamValue) =
+    //    match param with
+    //    | Value   v                 -> string v
+    //    | CvValue (_,v,_)           -> v
+    //    | WithCvUnitAccession (v,_) -> string v
+
+    static member getValueAsString (param:ParamValue) =
         match param with
         | Value   v                 -> string v
         | CvValue (_,v,_)           -> v
         | WithCvUnitAccession (v,_) -> string v
+
+    static member getValueAsInt (param:ParamValue) =
+        match param with
+        | Value   v                 -> v :?> int
+        | CvValue (_,v,_)           -> v |> int
+        | WithCvUnitAccession (v,_) -> v :?> int
+
+    static member getValueAsTerm (param:ParamValue) =
+        match param with
+        | Value   v              -> CvTerm.fromName (v |> string)
+        | CvValue term           -> term
+        | WithCvUnitAccession (v,_) -> CvTerm.fromName (v |> string)
+
 
     static member tryGetUnit  (param:ParamValue) : CvUnit option =
         match param with
