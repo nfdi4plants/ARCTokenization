1a54dfc3885dbdd8d416afd00368735b17c7b9c3
omaus <omaus@bio.uni-kl.de>
2023-04-18 21:20:38 +0200

    Add unit tests for `tryOfString` method

--- tests/ArcGraphModel.Tests/ArcTypeTests.fs
+++ tests/ArcGraphModel.Tests/ArcTypeTests.fs
@@ -126,3 +126,62 @@ module ArcType =
     //    [<Fact>]
     //    let ``Factor fails`` () =
     //        Assert.Throws<System.Exception> (fun _ -> testBuildingBlock5.ToNodeType() |> ignore)
+
+
+    module tryOfString =
+
+        [<Fact>]
+        let ``is Some`` () =
+            Assert.True (BuildingBlockType.tryOfString "test").IsSome
+
+        [<Fact>]
+        let ``creates Freetext correctly`` () =
+            Assert.Equal(Freetext "test" |> Some, BuildingBlockType.tryOfString "test")
+
+        [<Fact>]
+        let ``creates Source correctly`` () =
+            Assert.Equal(Some Source, BuildingBlockType.tryOfString "Source Name")
+
+        [<Fact>]
+        let ``creates Sample correctly`` () =
+            Assert.Equal(Some Sample, BuildingBlockType.tryOfString "Sample Name")
+
+        [<Fact>]
+        let ``creates RawDataFile correctly`` () =
+            Assert.Equal(Some RawDataFile, BuildingBlockType.tryOfString "Raw Data File")
+
+        [<Fact>]
+        let ``creates DerivedDataFile correctly`` () =
+            Assert.Equal(Some DerivedDataFile, BuildingBlockType.tryOfString "Derived Data File")
+
+        [<Fact>]
+        let ``creates Parameter correctly`` () =
+            Assert.Equal(Some Parameter, BuildingBlockType.tryOfString "Parameter")
+            Assert.Equal(Some Parameter, BuildingBlockType.tryOfString "Parameter Value")
+
+        [<Fact>]
+        let ``creates Characteristic correctly`` () =
+            Assert.Equal(Some Characteristic, BuildingBlockType.tryOfString "Characteristic")
+            Assert.Equal(Some Characteristic, BuildingBlockType.tryOfString "Characteristics")
+            Assert.Equal(Some Characteristic, BuildingBlockType.tryOfString "Characteristics Value")
+
+        [<Fact>]
+        let ``creates Factor correctly`` () =
+            Assert.Equal(Some Factor, BuildingBlockType.tryOfString "Factor")
+            Assert.Equal(Some Factor, BuildingBlockType.tryOfString "Factor Value")
+
+        [<Fact>]
+        let ``creates Component correctly`` () =
+            Assert.Equal(Some Component, BuildingBlockType.tryOfString "Component")
+
+        [<Fact>]
+        let ``creates Data correctly`` () =
+            Assert.Equal(Some Data, BuildingBlockType.tryOfString "Data File Name")
+
+        [<Fact>]
+        let ``creates ProtocolType correctly`` () =
+            Assert.Equal(Some ProtocolType, BuildingBlockType.tryOfString "Protocol Type")
+
+        [<Fact>]
+        let ``creates ProtocolREF correctly`` () =
+            Assert.Equal(Some ProtocolREF, BuildingBlockType.tryOfString "Protocol REF")
