ac5ab8c1836bb0c6fa718cf5ada67cf9bcf60d80
Heinrich Lukas Weil <hl.weil@t-online.de>
2023-04-27 19:05:11 +0200

    add attributes to tokenized containers

--- src/ArcGraphModel.IO/ISA/Tokenization.fs
+++ src/ArcGraphModel.IO/ISA/Tokenization.fs
@@ -25,14 +25,22 @@ module Tokenization =
     let parseLine (line : FsCell seq) =
         match line |> Seq.toList |> List.filter (fun c -> c.Value <> "") with
         | [] -> seq []
-        | [Container (ContainerBase.investigation) container]
         | [Container (ContainerBase.investigationContacts) container]
-        | [Container (ContainerBase.investigationPublication) container]
-        | [Container (ContainerBase.study) container]
+        | [Container (ContainerBase.investigationPublication) container] ->
+            container 
+            |> CvAttributeCollection.tryAddAttribute (CvParam(Terms.investigation,"")) 
+            |> ignore
+            [container] |> Seq.cast<ICvBase>
         | [Container (ContainerBase.studyContacts) container]
         | [Container (ContainerBase.studyAssays) container]
         | [Container (ContainerBase.studyDesignDescriptors) container]
         | [Container (ContainerBase.studyFactors) container] ->
+            container 
+            |> CvAttributeCollection.tryAddAttribute (CvParam(Terms.study,"")) 
+            |> ignore
+            [container] |> Seq.cast<ICvBase>
+        | [Container (ContainerBase.investigation) container]
+        | [Container (ContainerBase.study) container] -> 
             [container] |> Seq.cast<ICvBase>
         | line -> 
             convertTokens line
