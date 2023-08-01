59f52e4a2e8a5d06a1575c3c98226c1dd5b9f64a
Kevin Schneider <schneider.kev@outlook.de>
2023-08-01 08:58:43 +0200

    working empty investigation metadata test

---
+++ tests/ArcGraphModel.Tests/IntegrationTests/AssayMetadata.fs
@@ -0,0 +1,17 @@
+﻿module IntegrationTests.AssayMetadata
+
+open ControlledVocabulary
+open FsSpreadsheet
+open FsSpreadsheet.ExcelIO
+open ArcGraphModel
+open Xunit
+
+open TestUtils
+
+
+
+let allExpectedMetadataTerms = 
+    
+    Terms.AssayMetadata.cvTerms
+    |> List.skip 1 //(ignore root term)
+    |> List.map (fun p -> CvParam(p, ParamValue.Value "", []))
