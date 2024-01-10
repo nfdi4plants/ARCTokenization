560d46bfb11df5587f06a6da990bf49dad88334b
Kevin Schneider <schneider.kev@outlook.de>
2024-01-10 09:47:23 +0100

    fix #35 and #36

--- src/ARCTokenization/Workbook.fs
+++ src/ARCTokenization/Workbook.fs
@@ -8,33 +8,44 @@ open ARCTokenization.Terms
 module Workbook =
 
     let getInvestigationMetadataSheet (useLastSheetOnIncorrectName: bool) investigation =
-        try
-            FsWorkbook.getWorksheetByName Globals.INVESTIGATION_METADATA_SHEET_NAME investigation
-        with _ ->
+
+        match FsWorkbook.tryGetWorksheetByName Globals.INVESTIGATION_METADATA_SHEET_NAME investigation with
+        | Some ws -> ws
+        | None ->
             if useLastSheetOnIncorrectName then
-                FsWorkbook.getWorksheets investigation
-                |> Seq.last
+                match FsWorkbook.getWorksheets investigation |> Seq.tryLast with
+                | Some ws -> ws
+                | None -> failwith "No worksheets found in the workbook."
             else
-                failwith "No worksheet named 'isa_investigation' found in the workbook"
+                failwith $"No worksheet named {Globals.INVESTIGATION_METADATA_SHEET_NAME} found in the workbook."
+
 
     let getStudyMetadataSheet (useLastSheetOnIncorrectName: bool) study =
-        try
-            FsWorkbook.tryGetWorksheetByName Globals.STUDY_OBSOLETE_METADATA_SHEET_NAME study
-            |> Option.defaultValue (FsWorkbook.getWorksheetByName Globals.STUDY_METADATA_SHEET_NAME study)
-        with _ ->
-            if useLastSheetOnIncorrectName then
-                FsWorkbook.getWorksheets study
-                |> Seq.last
-            else
-                failwith $"No worksheet named {Globals.STUDY_OBSOLETE_METADATA_SHEET_NAME} or {Globals.STUDY_METADATA_SHEET_NAME} found in the workbook"
+
+        match FsWorkbook.tryGetWorksheetByName Globals.STUDY_OBSOLETE_METADATA_SHEET_NAME study with
+        | Some ws -> ws
+        | None ->
+            match FsWorkbook.tryGetWorksheetByName Globals.STUDY_METADATA_SHEET_NAME study with
+            | Some ws -> ws
+            | None ->
+                if useLastSheetOnIncorrectName then
+                    match FsWorkbook.getWorksheets study |> Seq.tryLast with
+                    | Some ws -> ws
+                    | None -> failwith "No worksheets found in the workbook."
+                else
+                    failwith $"No worksheet named {Globals.STUDY_OBSOLETE_METADATA_SHEET_NAME} or {Globals.STUDY_METADATA_SHEET_NAME} found in the workbook."
 
     let getAssayMetadataSheet (useLastSheetOnIncorrectName: bool) assay =
-        try
-            FsWorkbook.tryGetWorksheetByName Globals.ASSAY_OBSOLETE_METADATA_SHEET_NAME assay
-            |> Option.defaultValue (FsWorkbook.getWorksheetByName Globals.ASSAY_METADATA_SHEET_NAME assay)
-        with _ ->
-            if useLastSheetOnIncorrectName then
-                FsWorkbook.getWorksheets assay
-                |> Seq.last
-            else
-                failwith $"No worksheet named {Globals.ASSAY_OBSOLETE_METADATA_SHEET_NAME} or {Globals.ASSAY_METADATA_SHEET_NAME} found in the workbook"
+
+        match FsWorkbook.tryGetWorksheetByName Globals.ASSAY_OBSOLETE_METADATA_SHEET_NAME assay with
+        | Some ws -> ws
+        | None ->
+            match FsWorkbook.tryGetWorksheetByName Globals.ASSAY_METADATA_SHEET_NAME assay with
+            | Some ws -> ws
+            | None ->
+                if useLastSheetOnIncorrectName then
+                    match FsWorkbook.getWorksheets assay |> Seq.tryLast with
+                    | Some ws -> ws
+                    | None -> failwith "No worksheets found in the workbook."
+                else
+                    failwith $"No worksheet named {Globals.ASSAY_OBSOLETE_METADATA_SHEET_NAME} or {Globals.ASSAY_METADATA_SHEET_NAME} found in the workbook"
