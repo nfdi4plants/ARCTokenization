5d22ada911e935b49d3ab4743eea5971cdf43fad
Christopher Lux <lux-christopher@web.de>
2024-02-23 11:19:44 +0100

    Add new file and directory terms to AFSO
    Add file and directory terms used for AFSO

--- src/ARCTokenization/structural_ontologies/AFSO.fs
+++ src/ARCTokenization/structural_ontologies/AFSO.fs
@@ -24,3 +24,27 @@ module AFSO =
     let ``Path``           = CvTerm.create("AFSO:00000008","Path","AFSO")
     let ``File Path``      = CvTerm.create("AFSO:00000009","File Path","AFSO")
     let ``Directory Path`` = CvTerm.create("AFSO:00000010","Directory Path","AFSO")
+
+    // Top level Directories
+    let ``Studies Directory``   = CvTerm.create("AFSO:00000011","Studies Directory","AFSO")
+    let ``Assays Directory``    = CvTerm.create("AFSO:00000012","Assays Directory","AFSO")
+    let ``Runs Directory``      = CvTerm.create("AFSO:00000013","Runs Directory","AFSO")
+    let ``Workflows Directory`` = CvTerm.create("AFSO:00000014","Workflows Directory","AFSO")
+
+    // Sub level folders
+    let ``Study``   = CvTerm.create("AFSO:00000015","Study","AFSO")
+    let ``Assay``    = CvTerm.create("AFSO:00000016","Assay","AFSO")
+    let ``Run``      = CvTerm.create("AFSO:00000017","Run","AFSO")
+    let ``Workflow`` = CvTerm.create("AFSO:00000018","Workflow","AFSO")
+
+
+    // Isa FileTypes 
+    let ``Investigation ISA``   = CvTerm.create("AFSO:00000019","Investigation ISA","AFSO")
+    let ``Study ISA``           = CvTerm.create("AFSO:00000020","Study ISA","AFSO")
+    let ``Assay ISA``           = CvTerm.create("AFSO:00000021","Assay ISA","AFSO")
+    let ``Dataset ISA``         = CvTerm.create("AFSO:00000022","Dataset ISA","AFSO")
+
+    // Additional FileTypes
+    let ``CWL File``         = CvTerm.create("AFSO:00000023","CWL File","AFSO")
+    let ``YML File``         = CvTerm.create("AFSO:00000024","YML File","AFSO")
+ 
