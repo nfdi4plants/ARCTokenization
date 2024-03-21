ae61785dcb9c4624e6250fa380b6ca0bef309a65
omaus <omaus@bio.uni-kl.de>
2024-03-21 17:27:44 +0100

    Build OBO files and source files

--- src/ARCTokenization/structural_ontologies/AFSO.fs
+++ src/ARCTokenization/structural_ontologies/AFSO.fs
@@ -1,50 +1,54 @@
-﻿
-// This file should eventually be auto-generated from the respective obo files, to have a safe way of updating it from the same source.
-// For now, it is manually created and updated. It is not complete, just a collectAFSOn of terms needed for baseline WIP validatAFSOn
-
 namespace ARCTokenization.StructuralOntology
 
-open ControlledVocabulary
+    open ControlledVocabulary
 
-module AFSO =
+    module AFSO =
 
-    let ``File``      = CvTerm.create("AFSO:00000001","File","AFSO")
-    let ``Directory`` = CvTerm.create("AFSO:00000002","Directory","AFSO")
-    let ``File Type`` = CvTerm.create("AFSO:00000003","File Type","AFSO")
-    let ``Extension`` = CvTerm.create("AFSO:00000004","Extension","AFSO")
+        let File = CvTerm.create("AFSO:00000001", "File", "AFSO")
 
-    /// The name of the file.
-    let ``File Name``      = CvTerm.create("AFSO:00000005","File Name","AFSO")
-    let ``Directory Name`` = CvTerm.create("AFSO:00000006","Directory Name","AFSO")
-    
-    /// The full path of the directory or file.
-    let ``Full Name``      = CvTerm.create("AFSO:00000007","Full Name","AFSO")
+        let Directory = CvTerm.create("AFSO:00000002", "Directory", "AFSO")
 
-    /// The full path, relative path or a Universal Naming ConventAFSOn (UNC) path
-    let ``Path``           = CvTerm.create("AFSO:00000008","Path","AFSO")
-    let ``File Path``      = CvTerm.create("AFSO:00000009","File Path","AFSO")
-    let ``Directory Path`` = CvTerm.create("AFSO:00000010","Directory Path","AFSO")
+        let File_Type = CvTerm.create("AFSO:00000003", "File Type", "AFSO")
 
-    // Top level Directories
-    let ``Studies Directory``   = CvTerm.create("AFSO:00000011","Studies Directory","AFSO")
-    let ``Assays Directory``    = CvTerm.create("AFSO:00000012","Assays Directory","AFSO")
-    let ``Runs Directory``      = CvTerm.create("AFSO:00000013","Runs Directory","AFSO")
-    let ``Workflows Directory`` = CvTerm.create("AFSO:00000014","Workflows Directory","AFSO")
+        let Extension = CvTerm.create("AFSO:00000004", "Extension", "AFSO")
 
-    // Sub level folders
-    let ``Study Directory``   = CvTerm.create("AFSO:00000015","Study Directory","AFSO")
-    let ``Assay Directory``    = CvTerm.create("AFSO:00000016","Assay Directory","AFSO")
-    let ``Run Directory``      = CvTerm.create("AFSO:00000017","Run Directory","AFSO")
-    let ``Workflow Directory`` = CvTerm.create("AFSO:00000018","Workflow Directory","AFSO")
+        let File_Name = CvTerm.create("AFSO:00000005", "File Name", "AFSO")
 
+        let Directory_Name = CvTerm.create("AFSO:00000006", "Directory Name", "AFSO")
 
-    // Isa FileTypes 
-    let ``Investigation File``   = CvTerm.create("AFSO:00000019","Investigation File","AFSO")
-    let ``Study File``           = CvTerm.create("AFSO:00000020","Study File","AFSO")
-    let ``Assay File``           = CvTerm.create("AFSO:00000021","Assay File","AFSO")
-    let ``Dataset File``         = CvTerm.create("AFSO:00000022","Dataset File","AFSO")
+        let Full_Name = CvTerm.create("AFSO:00000007", "Full Name", "AFSO")
 
-    // Additional FileTypes
-    let ``CWL File``         = CvTerm.create("AFSO:00000023","CWL File","AFSO")
-    let ``YML File``         = CvTerm.create("AFSO:00000024","YML File","AFSO")
- 
+        let Path = CvTerm.create("AFSO:00000008", "Path", "AFSO")
+
+        let File_Path = CvTerm.create("AFSO:00000009", "File Path", "AFSO")
+
+        let Directory_Path = CvTerm.create("AFSO:00000010", "Directory Path", "AFSO")
+
+        let Studies_Directory = CvTerm.create("AFSO:00000011", "Studies Directory", "AFSO")
+
+        let Assays_Directory = CvTerm.create("AFSO:00000012", "Assays Directory", "AFSO")
+
+        let Runs_Directory = CvTerm.create("AFSO:00000013", "Runs Directory", "AFSO")
+
+        let Workflows_Directory = CvTerm.create("AFSO:00000014", "Workflows Directory", "AFSO")
+
+        let Study_Directory = CvTerm.create("AFSO:00000015", "Study Directory", "AFSO")
+
+        let Assay_Directory = CvTerm.create("AFSO:00000016", "Assay Directory", "AFSO")
+
+        let Run_Directory = CvTerm.create("AFSO:00000017", "Run Directory", "AFSO")
+
+        let Workflow_Directory = CvTerm.create("AFSO:00000018", "Workflow Directory", "AFSO")
+
+        let Investigation_File = CvTerm.create("AFSO:00000019", "Investigation File", "AFSO")
+
+        let Study_File = CvTerm.create("AFSO:00000020", "Study File", "AFSO")
+
+        let Assay_File = CvTerm.create("AFSO:00000021", "Assay File", "AFSO")
+
+        let Dataset_File = CvTerm.create("AFSO:00000022", "Dataset File", "AFSO")
+
+        let CWL_File = CvTerm.create("AFSO:00000023", "CWL File", "AFSO")
+
+        let YML_File = CvTerm.create("AFSO:00000024", "YML File", "AFSO")
+
--- src/ARCTokenization/structural_ontologies/ASSMSO.fs
+++ src/ARCTokenization/structural_ontologies/ASSMSO.fs
@@ -1,32 +1,90 @@
-﻿
-// This file should eventually be auto-generated from the respective obo files, to have a safe way of updating it from the same source.
-// For now, it is manually created and updated. It is not complete, just a collection of terms needed for baseline WIP validation
-
 namespace ARCTokenization.StructuralOntology
 
-open ControlledVocabulary
+    open ControlledVocabulary
 
-module ASSMSO = 
- 
-    module ``Assay Metadata`` =
-        let key =  CvTerm.create("ASSMSO:00000001","Assay Metadata","ASSMSO")
+    module ASSMSO =
 
-        module ``ASSAY`` =
-            let key =  CvTerm.create("ASSMSO:00000002","ASSAY","ASSMSO")
-        
-            let ``Assay File Name`` =  CvTerm.create("ASSMSO:00000019","Assay File Name","ASSMSO")
+        let Assay_Metadata = CvTerm.create("ASSMSO:00000001", "Assay Metadata", "ASSMSO")
 
-        module ``ASSAY PERFORMERS`` =
-            let key =  CvTerm.create("ASSMSO:00000020","ASSAY PERFORMERS","ASSMSO")
+        let ASSAY = CvTerm.create("ASSMSO:00000002", "ASSAY", "ASSMSO")
 
-            let ``Assay Performer Last Name`` =  CvTerm.create("ASSMSO:00000021","Assay Performer Last Name","ASSMSO")
-            let ``Assay Performer First Name`` =  CvTerm.create("ASSMSO:00000023","Assay Performer First Name","ASSMSO")
-            let ``Assay Performer Mid Initials`` =  CvTerm.create("ASSMSO:00000025","Assay Performer Mid Initials","ASSMSO")
-            let ``Assay Performer Email`` =  CvTerm.create("ASSMSO:00000027","Assay Performer Email","ASSMSO")
-            let ``Assay Performer Phone`` =  CvTerm.create("ASSMSO:00000029","Assay Performer Phone","ASSMSO")
-            let ``Assay Performer Fax`` =  CvTerm.create("ASSMSO:00000031","Assay Performer Fax","ASSMSO")
-            let ``Assay Performer Address`` =  CvTerm.create("ASSMSO:00000033","Assay Performer Address","ASSMSO")
-            let ``Assay Performer Affiliation`` =  CvTerm.create("ASSMSO:00000035","Assay Performer Affiliation","ASSMSO")
-            let ``Assay Performer Roles`` =  CvTerm.create("ASSMSO:00000037","Assay Performer Roles","ASSMSO")
-            let ``Assay Performer Roles Term Accession Number`` =  CvTerm.create("ASSMSO:00000039","Assay Performer Roles Term Accession Number","ASSMSO")
-            let ``Assay Performer Roles Term Source REF`` =  CvTerm.create("ASSMSO:00000041","Assay Performer Roles Term Source REF","ASSMSO")
+        let ASSAY_METADATA = CvTerm.create("ASSMSO:00000003", "ASSAY METADATA", "ASSMSO")
+
+        let Assay_Measurement_Type = CvTerm.create("ASSMSO:00000004", "Assay Measurement Type", "ASSMSO")
+
+        let Measurement_Type = CvTerm.create("ASSMSO:00000005", "Measurement Type", "ASSMSO")
+
+        let Assay_Measurement_Type_Term_Accession_Number = CvTerm.create("ASSMSO:00000006", "Assay Measurement Type Term Accession Number", "ASSMSO")
+
+        let Measurement_Type_Term_Accession_Number = CvTerm.create("ASSMSO:00000007", "Measurement Type Term Accession Number", "ASSMSO")
+
+        let Assay_Measurement_Type_Term_Source_REF = CvTerm.create("ASSMSO:00000008", "Assay Measurement Type Term Source REF", "ASSMSO")
+
+        let Measurement_Type_Term_Source_REF = CvTerm.create("ASSMSO:00000009", "Measurement Type Term Source REF", "ASSMSO")
+
+        let Technology_Type = CvTerm.create("ASSMSO:00000010", "Technology Type", "ASSMSO")
+
+        let Assay_Technology_Type = CvTerm.create("ASSMSO:00000011", "Assay Technology Type", "ASSMSO")
+
+        let Technology_Type_Term_Accession_Number = CvTerm.create("ASSMSO:00000012", "Technology Type Term Accession Number", "ASSMSO")
+
+        let Assay_Technology_Type_Term_Accession_Number = CvTerm.create("ASSMSO:00000013", "Assay Technology Type Term Accession Number", "ASSMSO")
+
+        let Technology_Type_Term_Source_REF = CvTerm.create("ASSMSO:00000014", "Technology Type Term Source REF", "ASSMSO")
+
+        let Assay_Technology_Type_Term_Source_REF = CvTerm.create("ASSMSO:00000015", "Assay Technology Type Term Source REF", "ASSMSO")
+
+        let Technology_Platform = CvTerm.create("ASSMSO:00000016", "Technology Platform", "ASSMSO")
+
+        let Assay_Technology_Platform = CvTerm.create("ASSMSO:00000017", "Assay Technology Platform", "ASSMSO")
+
+        let File_Name = CvTerm.create("ASSMSO:00000018", "File Name", "ASSMSO")
+
+        let Assay_File_Name = CvTerm.create("ASSMSO:00000019", "Assay File Name", "ASSMSO")
+
+        let ASSAY_PERFORMERS = CvTerm.create("ASSMSO:00000020", "ASSAY PERFORMERS", "ASSMSO")
+
+        let Assay_Performer_Last_Name = CvTerm.create("ASSMSO:00000021", "Assay Performer Last Name", "ASSMSO")
+
+        let Last_Name = CvTerm.create("ASSMSO:00000022", "Last Name", "ASSMSO")
+
+        let Assay_Performer_First_Name = CvTerm.create("ASSMSO:00000023", "Assay Performer First Name", "ASSMSO")
+
+        let First_Name = CvTerm.create("ASSMSO:00000024", "First Name", "ASSMSO")
+
+        let Assay_Performer_Mid_Initials = CvTerm.create("ASSMSO:00000025", "Assay Performer Mid Initials", "ASSMSO")
+
+        let Mid_Initials = CvTerm.create("ASSMSO:00000026", "Mid Initials", "ASSMSO")
+
+        let Assay_Performer_Email = CvTerm.create("ASSMSO:00000027", "Assay Performer Email", "ASSMSO")
+
+        let Email = CvTerm.create("ASSMSO:00000028", "Email", "ASSMSO")
+
+        let Assay_Performer_Phone = CvTerm.create("ASSMSO:00000029", "Assay Performer Phone", "ASSMSO")
+
+        let Phone = CvTerm.create("ASSMSO:00000030", "Phone", "ASSMSO")
+
+        let Assay_Performer_Fax = CvTerm.create("ASSMSO:00000031", "Assay Performer Fax", "ASSMSO")
+
+        let Fax = CvTerm.create("ASSMSO:00000032", "Fax", "ASSMSO")
+
+        let Assay_Performer_Address = CvTerm.create("ASSMSO:00000033", "Assay Performer Address", "ASSMSO")
+
+        let Address = CvTerm.create("ASSMSO:00000034", "Address", "ASSMSO")
+
+        let Assay_Performer_Affiliation = CvTerm.create("ASSMSO:00000035", "Assay Performer Affiliation", "ASSMSO")
+
+        let Affiliation = CvTerm.create("ASSMSO:00000036", "Affiliation", "ASSMSO")
+
+        let Assay_Performer_Roles = CvTerm.create("ASSMSO:00000037", "Assay Performer Roles", "ASSMSO")
+
+        let Roles = CvTerm.create("ASSMSO:00000038", "Roles", "ASSMSO")
+
+        let Assay_Performer_Roles_Term_Accession_Number = CvTerm.create("ASSMSO:00000039", "Assay Performer Roles Term Accession Number", "ASSMSO")
+
+        let Roles_Term_Accession_Number = CvTerm.create("ASSMSO:00000040", "Roles Term Accession Number", "ASSMSO")
+
+        let Assay_Performer_Roles_Term_Source_REF = CvTerm.create("ASSMSO:00000041", "Assay Performer Roles Term Source REF", "ASSMSO")
+
+        let Roles_Term_Source_REF = CvTerm.create("ASSMSO:00000042", "Roles Term Source REF", "ASSMSO")
+
